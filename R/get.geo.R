7d1241f814c0a1295c9fb025c03ba45a8a826e3d
AlexYachts <alessandro.panfili1@gmail.com>
2015-12-07 18:25:59 +0100

    Fix di get.geo

--- R/get.geo.R
+++ R/get.geo.R
@@ -1,95 +1,100 @@
 get.geo <- function(user, token){
-  
+  tryCatch(
   call <- get.user(user, token)
   
   if(!is.na(call[1])){
     if(call$data$counts$media!=0){
-    
-    n <- call$data$counts$media
-    
-    con = curl(paste0("https://api.instagram.com/v1/users/",user,"/media/recent?access_token=",token))
-    
-    call <- suppressWarnings(fromJSON(readLines(con), unexpected.escape = "keep"))
-    
-    close(con)
-    
-    if(length(call$pagination)==0){
-      df <- data.frame()
       
-      for(i in 1:length(call$data)){
-            df.tmp <- data.frame(
-            author = call$data[[i]]$user$id,
-            media = call$data[[i]]$id,
-            likes = call$data[[i]]$likes$count,
-            created.time = toString(as.POSIXct(as.numeric(call$data[[i]]$created_time), origin="1970-01-01")),
-            longitude = ifelse(!is.null(call$data[[i]]$location$longitude), call$data[[i]]$location$longitude, NA),
-            latitude = ifelse(!is.null(call$data[[i]]$location$latitude), call$data[[i]]$location$latitude, NA)
+      n <- call$data$counts$media
+      
+      con = curl(paste0("https://api.instagram.com/v1/users/",user,"/media/recent?access_token=",token,"&count=35"))
+      
+      call <- suppressWarnings(fromJSON(readLines(con), unexpected.escape = "keep"))
+      
+      close(con)
+      
+      cat(user)
+      
+      if(length(call$pagination)==0){
+        
+        df <- rbind.fill(lapply(call$data, function(x){
+          df.tmp <- data.frame(
+            author = ifelse(!is.null(x$user$id), x$user$id, NA),
+            media = ifelse(!is.null(x$id), x$id, NA),
+            likes = ifelse(!is.null(x$likes$count), x$likes$count, NA),
+            created.time = ifelse(!is.null(toString(as.POSIXct(as.numeric(x$created_time), origin="1970-01-01"))), toString(as.POSIXct(as.numeric(x$created_time), origin="1970-01-01")), NA),
+            longitude = ifelse(!is.null(x$location$longitude), x$location$longitude, NA),
+            latitude = ifelse(!is.null(x$location$latitude), x$location$latitude, NA)
           )
-          df <- rbind(df,df.tmp)
-          cat(paste("...", i, sep = ""))
+          df.tmp <- subset(df.tmp, !is.na(longitude)|!is.na(latitude))
+          row.names(df.tmp) <- NULL
+          return(df.tmp)
         }
-      df <- subset(df, !is.na(longitude)|!is.na(latitude))
-      row.names(df) <- NULL
-      return(df) 
+        )
+        )
+        return(df) 
+      }
+      else{
+        
+        df <- rbind.fill(lapply(call$data, function(x){
+          df.tmp <- data.frame(
+            author = ifelse(!is.null(x$user$id), x$user$id, NA),
+            media = ifelse(!is.null(x$id), x$id, NA),
+            likes = ifelse(!is.null(x$likes$count), x$likes$count, NA),
+            created.time = ifelse(!is.null(toString(as.POSIXct(as.numeric(x$created_time), origin="1970-01-01"))), toString(as.POSIXct(as.numeric(x$created_time), origin="1970-01-01")), NA),
+            longitude = ifelse(!is.null(x$location$longitude), x$location$longitude, NA),
+            latitude = ifelse(!is.null(x$location$latitude), x$location$latitude, NA)
+          )
+          df.tmp <- subset(df.tmp, !is.na(longitude)|!is.na(latitude))
+          row.names(df.tmp) <- NULL
+          return(df.tmp)
+        }
+        )
+        )
+        
+        n.retrieved <- length(call$data)
+        
+        while((n.retrieved<n) & (length(call$pagination)!=0)){
+          
+          next.url <- paste0(call$pagination$next_url,"&count=35")
+          con = curl(next.url)
+          call <- suppressWarnings(fromJSON(readLines(con), unexpected.escape = "keep"))
+          close(con)
+          
+          df2 <- rbind.fill(lapply(call$data, function(x){
+            df.tmp <- data.frame(
+              author = ifelse(!is.null(x$user$id), x$user$id, NA),
+              media = ifelse(!is.null(x$id), x$id, NA),
+              likes = ifelse(!is.null(x$likes$count), x$likes$count, NA),
+              created.time = ifelse(!is.null(toString(as.POSIXct(as.numeric(x$created_time), origin="1970-01-01"))), toString(as.POSIXct(as.numeric(x$created_time), origin="1970-01-01")), NA),
+              longitude = ifelse(!is.null(x$location$longitude), x$location$longitude, NA),
+              latitude = ifelse(!is.null(x$location$latitude), x$location$latitude, NA)
+            )
+            df.tmp <- subset(df.tmp, !is.na(longitude)|!is.na(latitude))
+            row.names(df.tmp) <- NULL
+            return(df.tmp)
+          }
+          )
+          )
+          
+          
+          df <- rbind.fill(df, df2)
+          n.retrieved.tmp <- length(call$data)
+          n.retrieved <- n.retrieved+n.retrieved.tmp
+          cat(paste("...", n.retrieved, sep = ""))
+        }
+        cat("... Done")
+        return(df)
+      }
     }
     else{
-      
-      df <- data.frame()
-      
-      
-      for(i in 1:length(call$data)){
-        df.tmp <- data.frame(
-          author = call$data[[i]]$user$id,
-          media = call$data[[i]]$id,
-          likes = call$data[[i]]$likes$count,
-          created.time = toString(as.POSIXct(as.numeric(call$data[[i]]$created_time), origin="1970-01-01")),
-          longitude = ifelse(!is.null(call$data[[i]]$location$longitude), call$data[[i]]$location$longitude, NA),
-          latitude = ifelse(!is.null(call$data[[i]]$location$latitude), call$data[[i]]$location$latitude, NA)
-        )
-        df <- rbind(df,df.tmp)
-      }
-      
-      n.retrieved <- length(call$data)
-      
-      while((n.retrieved<n) & (length(call$pagination)!=0)){
-        
-        next.url <- call$pagination$next_url
-        con = curl(next.url)
-        call <- suppressWarnings(fromJSON(readLines(con), unexpected.escape = "keep"))
-        close(con)
-              
-        df2 <- data.frame()
-        
-        for(i in 1:length(call$data)){
-          df.tmp <- data.frame(
-            author = call$data[[i]]$user$id,
-            media = call$data[[i]]$id,
-            likes = call$data[[i]]$likes$count,
-            created.time = toString(as.POSIXct(as.numeric(call$data[[i]]$created_time), origin="1970-01-01")),
-            longitude = ifelse(!is.null(call$data[[i]]$location$longitude), call$data[[i]]$location$longitude, NA),
-            latitude = ifelse(!is.null(call$data[[i]]$location$latitude), call$data[[i]]$location$latitude, NA)
-          )
-          df2 <- rbind(df2,df.tmp)
-        }
-        
-          
-        df <- rbind(df, df2)
-        n.retrieved.tmp <- length(call$data)
-        n.retrieved <- n.retrieved+n.retrieved.tmp
-        cat(paste("...", n.retrieved, sep = ""))
-      }
-      df <- subset(df, !is.na(longitude)|!is.na(latitude))
-      row.names(df) <- NULL
-      return(df)
+      return(NULL)
     }
+    
   }
   else{
     return(NULL)
   }
-  
-  }
-  else{
-    return(NULL)
-  }
-  
+  ,
+  error = function(e) NULL) 
 }
